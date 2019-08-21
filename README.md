21722ed12019fc869856e2d064821e56df6f76b5
Brendon Tse and Loewy Malkovich <loewy@>
2019-08-21 12:06:21 -0700

    add branching and forEach to find vowels

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 | Behavior | Input | Expected Output |
 |---|---|---|
 | Program will do nothing to non-alphabetical characters | 38% | 38% |
-|  If a word starts with a vowel, program will add "way" to the end | apple | appleway |
+| If a word starts with a vowel, program will add "way" to the end | apple | appleway |
 | If a word starts with a single consonant, program will move consonant to the end and add "ay" | water | aterway |
 | If a word starts with multiple consonants, program will move all consecutive consonants to the end and add "ay" | fresh | eshfray |
 | If a word starts with "y", program will treat "y" as a consonant | yogurt | ogurtyay |
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+
+// front end ui
 $(document).ready(function(){
-  
-})
+  $("form#pig-latin").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("#user-input").val();
+    console.log(userInput);
+    // business logic
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+    var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'];
+
+    vowels.forEach(function(vowel) {
+      if (userInput.charAt(0) === vowel) {
+        var output = (userInput + "way");
+        console.log(output);
+      }
+});
+    //
+    // } else if
+    // }
+    //  else {
+    //   alert("Please enter a sentence with alphabetical characters")
+    // };
+  });
+});
